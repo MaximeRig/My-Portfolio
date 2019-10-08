9bfa75077d1751e900af33addb3c348e19567bdc
MaximeRig <maxime.rigaud@ymail.com>
2019-10-08 21:29:34 +0200

    separation animation

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,8 +1,24 @@
 // == Import : npm
 import React from 'react';
+import Plx from 'react-plx';
 
 // == Import : local
 import './about.scss';
+
+const separationParallax = [
+  {
+    start: 'self',
+    end: 1000,
+    duration: 900,
+    properties: [
+      {
+        startValue: 0,
+        endValue: 90,
+        property: 'rotate',
+      },
+    ],
+  },
+];
 
 // == Composant
 const About = () => {
@@ -14,9 +30,12 @@ const About = () => {
         <p className="about-desc">Spécialisé dans l'écosytème <strong>Javascript</strong> avec <strong>React</strong>, je développe des interfaces en mobile first, axées sur la simplicité et l'accessibilité.</p>
         <p className="about-desc">Aujourd'hui, je recherche une entreprise dans laquelle m'épanouir afin d'apporter mon expérience et mes qualités.</p>
         <p className="about-desc">J'aime le développement, c'est la raison pour laquelle je continue de me former sur différentes technologies qui permettent d'être plus compétitifs sur le marché de l'emploi.</p>
-        <div className="box" />
       </section>
-      <div className="separateSection" />
+      <Plx
+        parallaxData={separationParallax}
+      >
+        <div className="separateSection" />
+      </Plx>
     </React.Fragment>
   );
 };
